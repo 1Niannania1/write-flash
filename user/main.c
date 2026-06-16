c8c19927b1d122550e3c1506965bfbbbea83d3a0
niannian <3312154119@qq.com>
2026-06-17 00:24:15 +0800

    无校验，有部分乱码，但可大量写入一本书。通过分包多次循环页写，done多次置位

--- user/main.c
+++ user/main.c
@@ -4,10 +4,7 @@
 #include "DEBUG.h"
 
 
-// uint8_t buffer[256];
-// uint16_t index=0;
-// uint8_t page_Size;
-// uint8_t page[5];
+
 
 //4MB 容量共有 16,384 页。
 int  main()
@@ -22,85 +19,85 @@ W25_init();
 USART_Init();
 
 
-// W25_eraser_all();//全片擦除
-// printf("ready\r\n");  
+W25_eraser_all();//全片擦除
+printf("ready\r\n");  
 
-// printf("page write count (must be 5bit):");
+printf("page write count (must be 5bit):");
 
-// //得到主机发送的count
-// for (int i = 0; i < 5; i++)
-// {
-//     while (out_cycle_buffer((uint8_t *)&page[i]) != 1)
-//     {
-//         // 等数据
-//     }
-// }
-// done=0;
+//得到主机发送的count
+for (int i = 0; i < 5; i++)
+{
+    while (out_cycle_buffer((uint8_t *)&page[i]) != 1)
+    {
+        // 等数据
+    }
+}
+done=0;
 
 
 
-// uint16_t page_count=0;
+uint16_t page_count=0;
 
-// for (uint8_t i = 0; i < 5; i++)
-// {
-//     if (page[i] >= '0' && page[i] <= '9')
-//     {
-//         page_count = page_count * 10 + (page[i] - '0');
-//     }
-// }
+for (uint8_t i = 0; i < 5; i++)
+{
+    if (page[i] >= '0' && page[i] <= '9')
+    {
+        page_count = page_count * 10 + (page[i] - '0');
+    }
+}
 
-// printf("page write=%d\n",page_count);
-// printf("plese transmit\n");
+printf("page write=%d\n",page_count);
+printf("plese transmit\n");
 
-// while (page_count)
-// {
-//    page_count--;
+while (page_count)
+{
+   page_count--;
    
 
 
-// while((done==0)||(tail!=head)) {
+while((done==0)||(tail!=head)) {
 
 
-// if (out_cycle_buffer(&buffer[index])==1)
-// {
-//   index++;
-// }
+if (out_cycle_buffer(&buffer[index])==1)
+{
+  index++;
+}
 
-// if (index==256)
-// {
+if (index==256)
+{
  
-//    W25_write_32addrress(address,buffer,256);
-//    index=0;
-//    address+=256;
-//    if (address==0x400000)
-//    {
-//   printf("no flash space\n");
-//    }
-//    printf("write one page\n");
+   W25_write_32addrress(address,buffer,256);
+   index=0;
+   address+=256;
+   if (address==0x400000)
+   {
+  printf("no flash space\n");
+   }
+   printf("write one page\n");
    
 
-// }
+}
 
     
 
 
-// }
-// if (index!=0)
-// {
+}
+if (index!=0)
+{
 
-// W25_write_32addrress(address,buffer,index);
-// break;
+W25_write_32addrress(address,buffer,index);
+break;
 
-// }
+}
 
-// //  多次写入
-// done=0;  
-// // tail=0;
-// // head=0;
-// index=0;
-// // address=0;
+//  多次写入
+done=0;  
+// tail=0;
+// head=0;
+index=0;
+// address=0;
 
-// }
+}
 
 debug_printf(" page write over\n");
 W25_read(0x00,0x00,0x20,0x00,read_cheak_buffer,256);
